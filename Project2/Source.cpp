33d1cceed0aa8399038e089359748a0be965f2d0
nopeajanis <84675406+nopeajanis@users.noreply.github.com>
2021-05-23 23:48:11 +0300

    hhh

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -4,6 +4,13 @@ using namespace std;
 int main()
 {
 	int a, b;
-	cout << "Hello, worlds!" << endl;
+	cout << "Hello, world!" << endl;
+	cout << "Hello, world!" << endl;
 	return 0;
+	/*
+	Blok
+	Blok
+	Blok
+	Blok
+	*/
 }
